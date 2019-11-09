8e7cd499b19c2609fa3908af03c17eabf69044d9
Sergey Tomashevich <Sergey.Tomashevich@zerto.com>
2019-11-09 13:20:43 +0200

    adding models

---
+++ Louis/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Louis.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Get(string Id)
+        {
+            return  $"suppose to get order with Id {Id}";
+        }
+    }
+}
---
+++ Louis/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louis.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
